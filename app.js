37fdc74f9e528a5e75097f079d34eb0464ddd057
PalashDhande07 <palashdhande01@gmail.com>
2023-10-22 23:41:51 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const fs = require("fs");   
+ 
+const bodyParser = require("body-parser");
+const app = express();  
+
+const port = 80;
